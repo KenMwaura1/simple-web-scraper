1a11587b9a8ce17a4cd538e3af752b99bb2d5e6d
KenMwaura1 <kemwaura@gmail.com>
2020-02-05 13:56:27 +0300

    removed unnecessary folders

--- base_sql.py
+++ base_sql.py
@@ -6,7 +6,10 @@ Created on Mon Jan 28 00:18:04 2020
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-engine =create_engine("postgres+psycopg2://ken:ken1738@localhost:5432/testdb",pool_pre_ping=True)
+
+engine = create_engine(
+    "postgres+psycopg2://ken:ken1738@localhost:5432/testdb", pool_pre_ping=True
+)
 engine.connect()
 print(engine)
 Session = sessionmaker(bind=engine)
--- scrape.py
+++ scrape.py
@@ -8,20 +8,20 @@ Created on Sun Sep 23 20:20:31 2019
 import requests
 from bs4 import BeautifulSoup
 
-#get the data 
-data = requests.get('https://www.umggaming.com/leaderboards')
+# get the data
+data = requests.get("https://www.umggaming.com/leaderboards")
 
-#load data into bs4
-soup = BeautifulSoup(data.text, 'html.parser')
+# load data into bs4
+soup = BeautifulSoup(data.text, "html.parser")
 
-leaderboard = soup.find('table',{'id':'leaderboard-table'})
-tbody = leaderboard.find('tbody')
+leaderboard = soup.find("table", {"id": "leaderboard-table"})
+tbody = leaderboard.find("tbody")
 
-for tr in tbody.find_all('tr'):
-    place = tr.find_all('td')[0].text.strip()
-    username = tr.find_all('td')[1].find_all('a')[1].text.strip()
-    xp = tr.find_all('td')[4].text.strip()
-    #print('position','username','xp', sep='\t')
-    print('Place', 'Username', 'XP', sep='\t')
-    print(place, username, xp, sep='\t')
-    #leaderboard-table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(5)
+for tr in tbody.find_all("tr"):
+    place = tr.find_all("td")[0].text.strip()
+    username = tr.find_all("td")[1].find_all("a")[1].text.strip()
+    xp = tr.find_all("td")[4].text.strip()
+    # print('position','username','xp', sep='\t')
+    print("Place", "Username", "XP", sep="\t")
+    print(place, username, xp, sep="\t")
+    # leaderboard-table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(5)
