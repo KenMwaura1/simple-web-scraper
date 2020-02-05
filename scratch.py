88f55bbca9b5c9dbd9b5f885a49715b77cac6980
KenMwaura1 <kemwaura@gmail.com>
2020-02-05 21:36:25 +0300

    Remove ignored files

--- scratch.py
+++
@@ -1,9 +0,0 @@
-try:
-        social:
-    except social[0] == '':
-        pass
-    social = tr.find_all("td")[3].find("div").find("ul").find_all("li")[0]
-    if social :
-        pass
-    else:
-        social = "pass"
