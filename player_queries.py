f2a0ffdb028e74cddcd4c8c2fb98c793403d1014
KenMwaura1 <kemwaura@gmail.com>
2020-02-09 18:36:50 +0300

    cleaned up the duplicate values

--- player_queries.py
+++ player_queries.py
@@ -18,7 +18,6 @@ session = Session()
 
 # 3 extract all players
 players = session.query(Earning_Player).all()
-print(players)
 for player in players:
     print(player.id, player.place, player.username, player.earnings)
     # 4 Delete duplicate values in the database
@@ -41,3 +40,24 @@ for x in q_duplicates:
     print("Will delete %s" % x)
     session.delete(x)
 session.commit()
+
+subq_1 = (
+    session.query(
+        Earning_Player.username, func.min(Earning_Player.id).label("min_id")
+    ).group_by(Earning_Player.username)
+).subquery("name_min_id")
+
+# query to find all duplicates
+player_duplicates = session.query(Earning_Player).join(
+    subq_1,
+    and_(
+        Earning_Player.username == subq_1.c.username,
+        Earning_Player.id != subq_1.c.min_id,
+    ),
+)
+
+for x in player_duplicates:
+    print("Will delete %s" % x)
+    session.delete(x)
+session.commit()
+
