d409cee51c38875a599e532e3ef43fc641a6d210
Ken_Mwaura1 <Ken_Mwaura1>
2023-01-07 00:25:25 +0300

    made tests into a py package

--- tests/test_players.py
+++ tests/test_players.py
@@ -1,14 +1,42 @@
 import pytest
 
-from player_sql import Player
-from top_earning_players_sql import Earning_Player
-from base_sql import Session, engine, Base
+from db.player_sql import Player
+from db.top_earning_players_sql import Earning_Player
+from db.base_sql import Session, engine, Base
+
+from db.player_factory_basic import PlayerFactory
+
+
+@pytest.fixture
+def player_func():
+    return PlayerFactory.build()
+
+
+@pytest.fixture
+def player_func2():
+    return PlayerFactory.build()
+
+def test_player(session, player_func):
+    session.add(player_func)
+    assert session.query(Player)
+    my_func_to_delete_Player(session, Player.id)
+    result = session.query(Player).one_or_none()
+    assert result is None
+
+def test_player2(session, player_func2):
+    session.add(player_func2)
+    assert session.query(Earning_Player)
+    my_func_to_delete_Player2(session, Earning_Player.id)
+    result = session.query(Earning_Player).one_or_none()
+    assert result is None
+
 
 @pytest.fixture(scope="module")
 def connection():
     connection = engine.connect()
     yield connection
     connection.close()
+
 
 #
 @pytest.fixture(scope="function")
@@ -34,17 +62,21 @@ def player_func():
     print(player)
     return player
 
+
 @pytest.fixture(scope="function")
 def player_func2():
     player = Earning_Player(username="zoo", place="2nd", earnings=23654)
     print(player)
     return player
 
+
 def my_func_to_delete_Player(session, Player_id):
     session.query(Player).filter(Player.id == Player_id).delete()
 
+
 def my_func_to_delete_Player2(session, Player_id):
     session.query(Earning_Player).filter(Earning_Player.id == Player_id).delete()
+
 
 def test_case(session, monkeypatch):
     test_request_payload = {"username": "something", "place": "2nd", "xp": 23654}
@@ -54,6 +86,7 @@ def test_case(session, monkeypatch):
     result = session.query(Player).one_or_none()
     assert result is None
 
+
 def test_player(session, player_func):
     session.add(player_func)
     assert session.query(Player)
@@ -61,13 +94,15 @@ def test_player(session, player_func):
     result = session.query(Player).one_or_none()
     assert result is None
 
+
 def test_case2(session, monkeypatch):
-    test_request_payload = {"username" : "something", "place" : "2nd", "earnings" : 23654}
+    test_request_payload = {"username": "something", "place": "2nd", "earnings": 23654}
     session.add(Earning_Player(username="zoo 2", place="2nd", earnings=23654))
     assert session.query(Earning_Player)
     my_func_to_delete_Player2(session, Earning_Player.id)
     result = session.query(Earning_Player).one_or_none()
     assert result is None
+
 
 def test_player2(session, player_func2):
     session.add(player_func2)
@@ -75,4 +110,3 @@ def test_player2(session, player_func2):
     my_func_to_delete_Player2(session, Earning_Player.id)
     result = session.query(Earning_Player).one_or_none()
     assert result is None
-
