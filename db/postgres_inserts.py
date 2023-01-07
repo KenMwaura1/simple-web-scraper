ee77f7b5ddd3ab808af70d0dab028030dd510ef5
Ken_Mwaura1 <Ken_Mwaura1>
2023-01-07 03:20:50 +0300

    fixed workflow

--- db/postgres_inserts.py
+++ db/postgres_inserts.py
@@ -2,16 +2,15 @@
 """
 @author: Ken Mwaura
 """
-from datetime import date
 
 from base_sql import Session, engine, Base
 from player_sql import Player
 
 # 1 generate database schema
-Base.metadata.create_all(engine)
+Base.metadata.create_all(engine, Base.metadata.tables.values(), checkfirst=True)
 
 # 2 Create a new session
 session = Session()
 
 # create players
-print(session)
+print(len(session.query(Player).all()))
