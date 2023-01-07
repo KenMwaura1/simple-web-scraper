bf8bcf5c1e64b67872b2cc481eae42062092e1ca
Ken_Mwaura1 <Ken_Mwaura1>
2023-01-07 03:31:27 +0300

    fixed workflow

--- db/postgres_inserts.py
+++ db/postgres_inserts.py
@@ -5,12 +5,17 @@
 
 from base_sql import Session, engine, Base
 from player_sql import Player
+from top_earning_players_sql import Earning_Player
 
 # 1 generate database schema
 Base.metadata.create_all(engine, Base.metadata.tables.values(), checkfirst=True)
 
 # 2 Create a new session
 session = Session()
+session.commit()
+session.close()
+
 
 # create players
 print(len(session.query(Player).all()))
+print(len(session.query(Earning_Player).all()))
